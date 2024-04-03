ade8347f69475272f85c0b98ed1e4f7e9c3e040b
diksha817 <129326352+diksha817@users.noreply.github.com>
2024-04-03 15:54:24 +0530

    Add files via upload

---
+++ RoleRequirement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace jwt_token
+{
+    public class RoleRequirement:IAuthorizationRequirement
+    {
+        public RoleRequirement(string roletype)
+        {
+            RoleType = roletype;
+        }
+        public string RoleType { get; set; }
+    }
+}
---
+++ RoleRequirementHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace jwt_token
+{
+    public class RoleRequirementHandler : AuthorizationHandler<RoleRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, RoleRequirement requirement)
+        {
+            if(!context.User.HasClaim(claim=>claim.Type=="roletype"))
+            {
+                return Task.CompletedTask;
+            }
+            if(context.User.FindFirst(c=>c.Type=="roletype").Value==requirement.RoleType)
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
